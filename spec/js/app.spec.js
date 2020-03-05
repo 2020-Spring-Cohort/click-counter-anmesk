b60e264030cc878b87c35a85a4493338808f2e0a
anmesk <an_mesk@live.com>
2020-03-05 10:15:24 -0500

    Established test descriptions for iteration #1

---
+++ spec/js/app.spec.js
@@ -0,0 +1,50 @@
+describe('Cookie Clicker Game', function(){
+
+    describe('clickCount', function(){
+        it('initial click count should be 0', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('clickCount', function(){
+        it('click count should increment by 1 from 0', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('initial companion count should be 0', function(){
+            expect(companionCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion should be purchasable at 100 clicks', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion should not be purchasable below 100 clicks', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion count should increment by 1 from 0', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion count cost should increase by 20% upon purchase', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('click count should increment by companion count value every second', function(){
+            expect(clickCount(0)).toBe('0');
+        });
+    })
+})
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+var clickCount = 0;
+
+var button = document.getElementById("cookieObject"),
+
+// button.onclick = function() {
+//     count += 1;
+//     button.innerHTML = "Cookie Object: " + count;
+// };
