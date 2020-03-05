41db66d76d9a59ca91480383209af8098d94e5f4
anmesk <an_mesk@live.com>
2020-03-05 11:45:16 -0500

    Variable initialization and incrementation tests are now passing

--- spec/js/app.spec.js
+++ spec/js/app.spec.js
@@ -1,44 +1,61 @@
 describe('Cookie Clicker Game', function(){
 
+    let sut;
+
+    beforeEach(function(){
+        sut = new CookieClicker();
+        sut.clickCount = 0;
+    })
+
     describe('clickCount', function(){
         it('initial click count should be 0', function(){
-            expect(clickCount(0)).toBe('0');
+            expect(sut.clickCount).toBe(0);
         });
     })
 
     describe('clickCount', function(){
         it('click count should increment by 1 from 0', function(){
-            expect(clickCount(0)).toBe('0');
+            sut.clickAction();
+            expect(sut.clickCount).toBe(1);
         });
     })
 
     describe('companionCount', function(){
         it('initial companion count should be 0', function(){
-            expect(companionCount(0)).toBe('0');
-        });
-    })
-
-    describe('companionCount', function(){
-        it('companion should be purchasable at 100 clicks', function(){
-            expect(clickCount(0)).toBe('0');
-        });
-    })
-
-    describe('companionCount', function(){
-        it('companion should not be purchasable below 100 clicks', function(){
-            expect(clickCount(0)).toBe('0');
+            expect(sut.companionCount).toBe(0);
         });
     })
 
     describe('companionCount', function(){
         it('companion count should increment by 1 from 0', function(){
+            sut.companionAdd();
+            expect(sut.companionCount).toBe(1);
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion should be purchasable at 100 clicks', function(){
+            if(clickCount >= 100){
+                // on-click to be implemented...
+                expect(companionCount(1)).toBe('1');
+            }
+            else expect(companionCount(0)).toBe('0');
+        });
+    })
+
+    describe('companionCount', function(){
+        it('companion should not be purchasable below 100 clicks', function(){
+            if(clickCount <= 100){
+                // on-click to be implemented...
+                expect(companionCount(0)).toBe('0');
+            }
             expect(clickCount(0)).toBe('0');
         });
     })
 
     describe('companionCount', function(){
-        it('companion count cost should increase by 20% upon purchase', function(){
-            expect(clickCount(0)).toBe('0');
+        it('companion count cost should increase by 10% upon purchase', function(){
+            expect(companionCount(0)).toBe('0');
         });
     })
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,38 @@
-var clickCount = 0;
+class CookieClicker{
 
-var button = document.getElementById("cookieObject"),
+    constructor(){
+        this.clickCount = 0;
+        this.companionCount = 0;
+        this.companionCost = 100;
+    }
+
+    clickCount = 0;
+    companionCount = 0;
+    companionCost = 100;
+
+    clickAction(){
+        this.clickCount++;
+    }
+
+    companionAdd(){
+        this.companionCount++;
+        // if(clickCount >= 100){
+        //     this.clickCount - this.companionCost == this.clickCount;
+        //     this.companionCount++;
+        // }
+    }
+}
+
+let cookieClicker = new CookieClicker();
+cookieClicker.clickAction();
+cookieClicker.companionAdd();
+// cookieClicker.setClickCountToZero();
+// cookieClicker.setCompanionCountToZero();
+
+
+// var clickCount = 0;
+
+// var button = document.getElementById("cookieObject"),
 
 // button.onclick = function() {
 //     count += 1;
