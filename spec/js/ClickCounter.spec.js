8777e08823a4eb21a1b23949f09b60a45f4c3e4d
anmesk <an_mesk@live.com>
2020-03-08 18:50:19 -0400

    Started establishing iteration #3 tests

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -1,8 +1,8 @@
-describe('Cookie Clicker Game', function(){
+describe('Cookie Clicker Game', () => {
 
     let sut;
 
-    beforeEach(function(){
+    beforeEach(() => {
         sut = new CookieClicker();
         sut.clickCount = 0;
         sut.companionCount = 0;
@@ -12,132 +12,199 @@ describe('Cookie Clicker Game', function(){
         sut.compounderCost = 10;
     })
 
-    describe('Click Tests', function(){
-        describe('Upon initializing the game, the click count should be set to 0', function(){
-            it('Initial clickCount should be 0', function(){
+    // Iteration #1 Tests
+    describe('Click Tests', () => {
+        describe('Upon initializing the game, the click count should be set to 0', () => {
+            it('Initial clickCount should be 0', () => {
                 expect(sut.clickCount).toBe(0);
-            });
+            })
         })
     
-        describe('When a click action is carried out, the click count should increase by 1', function(){
-            it('clickCount should increment by 1 from 0', function(){
+        describe('When a click action is carried out, the click count should increase by 1', () => {
+            it('clickCount should increment by 1 from 0', () => {
                 sut.clickAction();
                 expect(sut.clickCount).toBe(1);
-            });
+            })
         })
 
-        describe('When a Companion is purchased, the click count should decrease by its cost value', function(){
-            it('Click count cost should decrease by 100 upon Companion purchase', function(){
+        describe('When a Companion is purchased, the click count should decrease by its cost value', () => {
+            it('Click count cost should decrease by 100 upon Companion purchase', () => {
                 sut.clickCount = 100;
                 sut.companionBuy();
                 expect(sut.clickCount).toBe(0);
-            });
+            })
         })
     })
     
 
-    describe('Companion Tests', function(){
-        describe('Upon initializing the game, the Companion count should be set to 0', function(){
-            it('Initial companionCount should be 0', function(){
+    describe('Companion Tests', () => {
+        describe('Upon initializing the game, the Companion count should be set to 0', () => {
+            it('Initial companionCount should be 0', () => {
                 expect(sut.companionCount).toBe(0);
-            });
+            })
         })
     
-        describe('When a Companion add action is carried out, the Companion count should increase by 1', function(){
-            it('companionCount should increment by 1 from 0', function(){
+        describe('When a Companion add action is carried out, the Companion count should increase by 1', () => {
+            it('companionCount should increment by 1 from 0', () => {
                 sut.companionAdd();
                 expect(sut.companionCount).toBe(1);
-            });
+            })
         })
     
-        describe('When the click count reaches 100, the Companion count should increase by 1', function(){
-            it('Companion should be purchasable at 100 clicks', function(){
+        describe('When the click count reaches 100, the Companion count should increase by 1', () => {
+            it('Companion should be purchasable at 100 clicks', () => {
                 sut.clickCount = 100;
                 sut.companionAdd();
                 expect(sut.companionCount).toBe(1);
-            });
+            })
         })
     
-        describe('When the click count is below 100, the Companion count should not be able to increase', function(){
-            it('Companion should not be purchasable below 100 clicks', function(){
+        describe('When the click count is below 100, the Companion count should not be able to increase', () => {
+            it('Companion should not be purchasable below 100 clicks', () => {
                 sut.clickCount = 0;
                 sut.companionBuy();
                 expect(sut.companionCount).toBe(0);
-            });
+            })
         })
     
-        describe('When a Companion is added, its purchase cost should increase by 10%', function(){
-            it('Companion count cost should increase by 10% upon purchase', function(){
+        describe('When a Companion is added, its purchase cost should increase by 10%', () => {
+            it('Companion count cost should increase by 10% upon purchase', () => {
                 sut.clickCount = 100;
                 sut.companionBuy();
                 expect(sut.companionCost).toBe(110);
-            });
+            })
         })
 
-        // describe('When a Companion is present, the click count should increase every second', function(){
-        //     it('Click count should increment by companion count value every second', function(){
+        // describe('When a Companion is present, the click count should increase every second', () => {
+        //     it('Click count should increment by companion count value every second', () => {
         //         expect(clickCount(0)).toBe('0');
-        //     });
+        //     })
         // })
     })
 
-
-    describe('Compounder Tests', function(){
-        describe('Upon initializing the game, the compounder count should be set to 0', function(){
-            it('Initial compounderCount should be 0', function(){
+    // Iteration #2 Tests
+    describe('Compounder Tests', () => {
+        describe('Upon initializing the game, the compounder count should be set to 0', () => {
+            it('Initial compounderCount should be 0', () => {
                 expect(sut.compounderCount).toBe(0);
-            });
+            })
         })
 
-        describe('When a Compounder add action is carried out, the Compounder count should increase by 1', function(){
-            it('compounderCount should increment by 1 from 0', function(){
+        describe('When a Compounder add action is carried out, the Compounder count should increase by 1', () => {
+            it('compounderCount should increment by 1 from 0', () => {
                 sut.compounderAdd();
                 expect(sut.compounderCount).toBe(1);
-            });
+            })
         })
 
-        describe('When the click count reaches 10, the Compounder count should increase by 1', function(){
-            it('Compounder should be purchasable at 10 clicks', function(){
+        describe('When the click count reaches 10, the Compounder count should increase by 1', () => {
+            it('Compounder should be purchasable at 10 clicks', () => {
                 sut.clickCount = 10;
                 sut.compounderAdd();
                 expect(sut.compounderCount).toBe(1);
-            });
+            })
         })
     
-        describe('When the click count is below 10, the Compounder count should not be able to increase', function(){
-            it('Compounder should not be purchasable below 10 clicks', function(){
+        describe('When the click count is below 10, the Compounder count should not be able to increase', () => {
+            it('Compounder should not be purchasable below 10 clicks', () => {
                 sut.clickCount = 0;
                 sut.compounderBuy();
                 expect(sut.compounderCount).toBe(0);
-            });
+            })
         })
     
-        describe('When a Compounder is added, its purchase cost should increase by 10%', function(){
-            it('Compounder count cost should increase by 10% upon purchase', function(){
+        describe('When a Compounder is added, its purchase cost should increase by 10%', () => {
+            it('Compounder count cost should increase by 10% upon purchase', () => {
                 sut.clickCount = 10;
                 sut.compounderBuy();
                 expect(sut.compounderCost).toBe(11);
-            });
+            })
         })
 
-        describe('When a Compounder is added, the value of a click should increase by 1.2x', function(){
-            it('With 1 Compounder present, the value of a click should increase by 1.2x', function(){
+        describe('When a Compounder is added, the value of a click should increase by 1.2x', () => {
+            it('With 1 Compounder present, the value of a click should increase by 1.2x', () => {
                 sut.clickValue = 1;
                 sut.clickCount = 10;
                 sut.compounderCount = 0;
                 sut.compounderBuy();
                 expect(sut.clickValue).toBe(1.2);
-            });
+            })
         })
 
-        describe('When more Compounders are added, the value of a click should increase by 1.2x to the xth power', function(){
-            it('With x Compounders, the value of a click should increase by 1.2x to the xth power', function(){
+        describe('When more Compounders are added, the value of a click should increase by 1.2x to the xth power', () => {
+            it('With x Compounders, the value of a click should increase by 1.2x to the xth power', () => {
                 sut.clickValue = 1.2;
                 sut.clickCount = 11;
                 sut.compounderCount = 1;
                 sut.compounderBuy();
                 expect(sut.clickValue).toBe(1.44);
-            });
+            })
+        })
+    })
+})
+
+// Iteration #3 Tests
+describe("Cookie Clicker DOM Manipulation Tests", () => {
+    testClicker = new ClickCounter
+    let testCookieButton
+    let testCookieCount
+    let testCompanionButton
+    let testCompanionCount
+    let testCompanionCost
+    let testCompounderButton
+    let testCompounderCount
+    let testCompounderCost
+    
+    beforeEach(() => {
+        testCookieButton = document.createElement('button');
+        testCookieCount = document.createElement('div');
+        testCompanionButton = document.createElement('button');
+        testCompanionCount = document.createElement('div');
+        testCompanionCost = document.createElement('div');
+        testCompounderButton = document.createElement('button');
+        testCompounderCount = document.createElement('div');
+        testCompounderCost = document.createElement('div');
+    })
+
+    describe("Cookie Counter Tests", () => {
+        describe("Cookie Counter Update Test - Initialization", () => {
+            it("Cookie button test count should be initialized with a value of 0 (no clicks)", () => {
+                expect(testCookieCount.innerText).toBe('0');
+            })
+        })
+    
+        describe("Cookie Counter Update Test - Value Update Upon Click", () => {
+            it("Cookie button test count should read 1 after user's first button click", () => {
+                expect(testCookieCount.innerText).toBe('1');
+            })
+        })
+    })
+    
+    describe("Companion Counter Tests", () => {
+        describe("Companion Counter Update Test - Initialization", () => {
+            it("Companion button test count should be initialized with a value of 0 (no clicks)", () => {
+                expect(testCompanionCount.innerText).toBe('0');
+            })
+        })
+    
+        describe("Companion Counter Update Test - Value Update Upon Click", () => {
+            it("Companion button test count should read 1 after user's first button click", () => {
+                expect(testCompanionCount.innerText).toBe('1');
+            })
+        })
+    })
+
+    describe("Compounder Counter Tests", () => {
+        describe("Compounder Counter Update Test - Initialization", () => {
+            it("Compounder button test count should be initialized with a value of 0 (no clicks)", () => {
+                expect(testCompounderCount.innerText).toBe('0');
+            })
+        })
+    
+        describe("Compounder Counter Update Test - Value Update Upon Click", () => {
+            it("Compounder test count should read 1 after user's first button click", () => {
+                expect(testCompounderCount.innerText).toBe('1');
+            })
         })
     })
 })
